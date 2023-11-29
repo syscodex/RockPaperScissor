91975d050b2f6876c4f22f8ebd8a580c63ccd3bf
Vincent <77495110+syscodex@users.noreply.github.com>
2023-11-29 23:49:39 +0800

    Fix bug on prompt function to get the player input handle case sensitive inputs by converting the input to UPPERCASE

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+
+function playRound(playerSelection, computerSelection) {
+    
+    console.log("Player chose: " + playerSelection);
+    console.log("Computer chose: " + computerSelection);
+    
+    // It is accepting string even though its not on the choices
+    if (
+        (playerSelection === "ROCK" && computerSelection === "SCISSOR") ||
+        (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+        (playerSelection === "SCISSOR" && computerSelection === "PAPER")
+    ) {
+        return "You win!";
+    } else if (
+        (playerSelection === "ROCK" && computerSelection === "PAPER") ||
+        (playerSelection === "PAPER" && computerSelection === "SCISSOR") ||
+        (playerSelection === "SCISSOR" && computerSelection === "ROCK")
+    ) {
+        return "You lose!";
+    } else {
+        return "its a draw!";
+    }
+}
+
 function getComputerChoice (){
+    
     //Array of choices which is R, P , S
-    let computerChoice = ["Rock" , "Paper", "Scissors"];
+    let computerChoice = ["ROCK" , "PAPER", "SCISSOR"];
 
     //Calculate a random index on the array
     let randomlyPick = Math.floor(Math.random() * computerChoice.length)
@@ -10,32 +35,13 @@ function getComputerChoice (){
 
 }
 
-function playRound(playerSelection, computerSelection) {
-    console.log("Player chose: " + playerSelection);
-    console.log("Computer chose: " + computerSelection);
 
-    if (
-        (playerSelection === "Rock" && computerSelection === "Scissors") ||
-        (playerSelection === "Paper" && computerSelection === "Rock") ||
-        (playerSelection === "Scissors" && computerSelection === "Paper")
-    ) {
-        console.log("You win!");
-    } else if (
-        (playerSelection === "Rock" && computerSelection === "Paper") ||
-        (playerSelection === "Paper" && computerSelection === "Scissors") ||
-        (playerSelection === "Scissors" && computerSelection === "Rock")
-    ) {
-        console.log("You lose!");
-    } else {
-        console.log("It's a draw!");
-    }
-}
-
-
-let playerChoice = "Rock";
 let computerChoice = getComputerChoice();
+let playerChoice = prompt("choose between rock paper and scissor").toUpperCase();
 
-playRound(playerChoice, computerChoice);
+
+let result = playRound(playerChoice, computerChoice);
+console.log(result);
 
 
 
